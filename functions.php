07d4b445618fcb173e9f9371b9b25100e5c23101
marketermatt <matt@marketermatt.com>
2015-02-02 01:34:07 -0500

    Added remove_action / add_action for single-deal template files

--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
 <?php
 define('PTS_DOMAIN', 'pts');
 require_once( get_template_directory() . '/framework/init.php' );
+
+/* Rearranging Deal Page */
+
+remove_action( 'wps_deals_single_header_content', 'wps_deals_single_header_left', 30 );
+remove_action( 'wps_deals_single_header_content', 'wps_deals_single_header_right', 35 );
+
+add_action( 'wps_deals_single_header_content', 'wps_deals_single_header_left', 35 );
+add_action( 'wps_deals_single_header_content', 'wps_deals_single_header_right', 30 );
+
