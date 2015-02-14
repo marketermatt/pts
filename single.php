b98d0b6f7a9f9dd2dea44cd3175af15a4ca0ad38
marketermatt <matt@marketermatt.com>
2015-02-14 15:38:50 -0500

    Gagan Fix to Deal Button Shortcode and Single Blog Page

--- single.php
+++ single.php
@@ -9,40 +9,66 @@
  */
 
 get_header(); ?>
-<div class="content_back">
-<div class='title-area'>
+
+<div class='content_back title-area'>
     <h2 class="page-title"><?php the_title(); ?></h2>
-    <div class="entry-meta"><?php echo pts_posted_on(); ?></div>
 </div>
 
 <div class="container-wrap">
     <div class="container">
-        <div class="row">             <div class="content-area col-xs-12 col-sm-8 col-md-9 hidden-overflow">
+        <div class="row">
+			
+            <div class="content-area col-xs-12 col-sm-8 col-md-9 hidden-overflow">
+                <div class="post-list">
+				<div class="list-item"> 
+                    <div class="list-content">
+                        <a href="<? the_permalink(); ?>"><h4><?php echo get_the_title(); ?></h4></a>
+                        <div class="clearfix"></div>
+                        <span>Posted on: <?php the_date(); ?></span> <?php echo do_shortcode('[display_rating_result post_id="'.get_the_ID().'" show_rich_snippets="true" show_count="false"]'); ?>
+                        <div class="hr"></div>
+                        <?php get_template_part('template/post','format'); ?>
                 <?php
-                    if ( have_posts() ) :
-                        // Start the Loop.
-                        while ( have_posts() ) : the_post();
-                            the_content();
+                            $comment_count = wp_count_comments( post_id );
+                            $catlist='';
+                            $categories = get_the_category();
+                            foreach($categories as $cat){
+                                $catlist .= '<a href="'.get_category_link($cat->term_id ).'">'.$cat->name.'</a>, ';
+                            }
                         ?>
-                        <div class="tags">
-                            <?php the_tags(); ?>
                         </div>
-                <?php
-                        endwhile;
-                    endif;
-                ?>
-                <?php
-                    ob_start();
-                    comments_template('', true);
-                    $output = ob_get_contents();
-                    ob_end_clean();
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-4 no-15-right">
+                            <div class="loop-grad">
+                                <div class="ci icon-area"></div>
+                                <div class="loop-lower-content">
+                                    <h6>Comments</h6>
+                                    <div><small><?php echo $comment_count->approved; ?> Comments</small></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 no-15">
+                            <div class="loop-grad">
+                                <div class="cat icon-area"></div>
+                                <div class="loop-lower-content">
+                                    <h6>Categories</h6>
+                                    <div><small><?php echo rtrim($catlist,','); ?></small></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 no-15-left">
+                            <div class="loop-grad">
 
-                    $output = str_replace('type="text"','type="text" class="form-control"',$output);
-                    $output = str_replace('<textarea','<textarea class="form-control"',$output);
-                    $output = str_replace('class="submit"','class="btn btn-default"',$output);
+                            <div class="icon-area author-area"></div>
+                                <div class="loop-lower-content">
+                                    <h6>Author</h6>
+                                    <div><small><?php echo get_the_author(); ?></small></div>
+                                </div>
 
-                    echo '<br/>'.$output;
-                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+				</div>				
             </div>
             <div class="sidebar-area col-xs-12 col-sm-4 col-md-3 hidden-overflow">
                 <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('single_post_widget') ) : else : ?>
@@ -50,6 +76,5 @@ get_header(); ?>
             </div>
         </div>
     </div>
-</div>
 </div>
 <?php get_footer(); ?>
